40afadd66d03a8181b0ced41f8699265b4823d9b
rawatmanoj <42403468+rawatmanoj@users.noreply.github.com>
2020-06-09 21:25:17 +0530

    update

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB succesful');
+  });
+
+//console.log(process.env.NODE_ENV);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
